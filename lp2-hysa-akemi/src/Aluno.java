36f925e468ef57eda29b5df7165b3b15d59f7ebb
hysabelly <90413604+hysabelly@users.noreply.github.com>
2022-06-10 01:24:17 -0300

    Update Aluno.java

--- lp2-hysa-akemi/src/Aluno.java
+++ lp2-hysa-akemi/src/Aluno.java
@@ -4,25 +4,35 @@ Akemi Alice - SP304484X
 Hysabelly Nunes - SP3044742 
 */
 
+// variáveis
 public class Aluno {
 	private String nome;
 	private String numeroMatricula;
 	private String instrutor;
 	private String turno;
+  private String curso;
+  private String instituicao;
 	private List<String> materias; 
 	private Set<Aluno> alunos = new HashSet<>();
-	
-	  Aluno(String nome, String numeroMatricula, String instrutor, String turno) {
+
+  // exceção em java 
+  Aluno(String nome, String numeroMatricula, String instrutor, String curso, String turno, String instituicao) {
 		if (nome == null) {
 			throw new NullPointerException("Nome não pode ser nulo");
 		}
 	    this.nome = nome;
 	    this.turno = turno;
+    	this.curso = curso;
+      this.instituicao = instituicao;
 	    this.numeroMatricula = numeroMatricula;
 	    this.instrutor = instrutor;
 	    this.materias = new ArrayList<String>();
 	    }
-	  
+
+  	public String getNome() {
+		return nome;
+	}
+    // adicionado matérias 
 	  public void addMaterias(List<String> materiasNovas) {
 			  materias.addAll(materiasNovas); 
 		  }
@@ -30,7 +40,8 @@ public class Aluno {
 	  public void addMaterias(String materia) {
 		    materias.add(materia);
 		  }
-
+  
+    // remover matérias 
 	  public void removerMaterias() {
 			  materias.clear();
 		  }
@@ -38,30 +49,49 @@ public class Aluno {
 	  public List<String> getMaterias() {
 		    return materias;
 		  }
-	
+
+    // quantidade de matérias
 	  public int getTamMat() {
 		    return materias.size();
 		  }
-	
+
+    // nome do aluno
 	  public String toString() {
 		   return this.nome;
 		  }
-		  
+   
+    // número da matrícula
 	  public String getNumeroMatricula() {
 		    return this.numeroMatricula;
 		  }
-	  
+
+    // instrutor da matéria
 	  public String getInstrutor() {
 		    return this.instrutor;
 		  }
 
+    // turno do aluno
 	  public String getTurno() {
 		    return this.turno;
 		  }
-	  
+
+    // instituição do aluno
+	  public String getInstituicao() {
+		    return this.instituicao;
+		  }
+
+    // curso do aluno
+	  public String getCurso() {
+		    return this.curso;
+		  }
+
+    // saber se o aluno está matriculado 
 		public boolean estaMatriculado(Aluno aluno) {
 			return this.alunos.contains(aluno);
 		  }
 
-		  }
-	
+    // buscando matéria posiçao 0
+  		public String buscar(int index) {
+			return materias.get(index);
+      }
+		}
