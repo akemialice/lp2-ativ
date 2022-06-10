7509c63dff0070303ff2270c072a8881e2995033
hysabelly <90413604+hysabelly@users.noreply.github.com>
2022-06-10 01:22:04 -0300

    Update Main.java

--- lp2-hysa-akemi/src/Main.java
+++ lp2-hysa-akemi/src/Main.java
@@ -3,48 +3,67 @@ import java.util.*;
 public class Main {
   public static void main(String args[]) {
 
-    Aluno aluno1 = new Aluno("Akemi Alice", "3333", "Márcio", "Manhã");
-    aluno1.addMaterias("FIS"); 
-    aluno1.addMaterias("MAT");
+    //declarando aluno
+    Aluno aluno1 = new Aluno("Akemi Alice", "12345", "Márcio","Medicina Veterinária", "Manhã", "Universidade Federal de São Paulo");
+    aluno1.addMaterias("Anatomia dos seres vivos"); 
+    aluno1.addMaterias("Estudo dos seres vivos");
     
-    Aluno aluno2 = new Aluno("Hysabelly Nunes", "1234","Júlia", "Noite");
-    aluno2.addMaterias("LP2"); 
-    aluno2.addMaterias("LP3");
-    aluno2.addMaterias("TPO");
-
-    System.out.println("====================");
+    Aluno aluno2 = new Aluno("Hysabelly Nunes", "67890","Júlia","Nutrição", "Noite", "Universidade Federal de São Paulo");
+    aluno2.addMaterias("Planejamento Alimentar"); 
+    aluno2.addMaterias("Técnica Dietética");
+    aluno2.addMaterias("Educação Nutricional");
+    
+    //exibindo os alunos cadastrados
     Map<String, Aluno> mapa = new TreeMap<String, Aluno>();
     mapa.put("Akemi Alice", aluno1);
     mapa.put("Hysabelly Nunes", aluno2);
     System.out.println(mapa);
 
-    System.out.println("====================");
+    System.out.println("\n");
+    System.out.println("========== Ficha Cadastral ==========");
+        System.out.println("> Universidade Federal de São Paulo <");
+  
+    //dados dos alunos: nome, matéria, matrícula, instrutor, curso, turno e instituição
+    System.out.println("\n");
+    
     Collection<Aluno> alunos = mapa.values(); 
     for (Aluno aluno : alunos) {
       System.out.println("Nome do aluno(a): " + aluno);
       System.out.println("Número da matricula: " + aluno.getNumeroMatricula());
-      System.out.println("Instrutor: " + aluno.getInstrutor());
+      System.out.println("Instrutor da matéria: " + aluno.getInstrutor());
+      System.out.println("Cursando: " + aluno.getCurso());
       System.out.println("Turno: " + aluno.getTurno());
+      System.out.println("Matérias inscritas:" + aluno.getMaterias());
+      System.out.println("Instituição de ensino: " + aluno.getInstituicao());
+      System.out.println("Quantidade de matérias inscrito: " + aluno.getTamMat());
+      //buscando por index - matéria posição 0
+       System.out.println("1º matéria inscrita: " + aluno.buscar(0));
+      System.out.println("\n");
     }
     
+    //removendo aluno: Akemi Alice
     while(alunos.iterator().hasNext()) {
       if(alunos.contains(aluno1))
         alunos.remove(aluno1);
       break;
-      
     }
-    
-    System.out.println("========== Removendo... ==========");
-    System.out.printf("Aluno(a) removido: "+ aluno1.toString());
-   
-    
-    System.out.println("\n==================== ");
-    System.out.print("Matérias do(a) aluno(a) 1 -> " + aluno1.toString() +": "); 
-    Iterator iterator01 = aluno1.getMaterias().iterator();
-    
-    System.out.print("Matérias do(a) aluno(a) 2 -> " + aluno2.toString() +": "); 
-    Iterator iterator02 = aluno2.getMaterias().iterator();
-    
 
+    System.out.println("===== Alunos(a) removidos: =====");
+    System.out.println("\n");
+    System.out.printf(aluno1.toString());
+
+    //exibindo lista atualizada de alunos
+    System.out.println("\n");
+    System.out.println("===== Lista atualizada de alunos(a) =====");
+    System.out.println("\n");
+    System.out.println(mapa);
+    System.out.println("\n");
+
+    System.out.println("===== Busca por alunos(a): =====");
+    System.out.println("\n");
+      
+    System.out.println("O aluno " + aluno1.getNome() + " está matriculado?");
+ 	System.out.println(aluno1.estaMatriculado(aluno1));
+	
   }
 }
